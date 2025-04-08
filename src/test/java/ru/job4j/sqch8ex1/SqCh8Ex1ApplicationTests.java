18bb76b03f4c30a36901173d485ec4488248e5f6
alkrivosheev <alkrivosheev@gmail.com>
2025-04-08 12:59:55 +0300

    new Web proj

---
+++ src/test/java/ru/job4j/sqch8ex1/SqCh8Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.sqch8ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh8Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
