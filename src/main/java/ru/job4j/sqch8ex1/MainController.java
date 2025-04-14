61d660f92d542a9a94b36d98080828e46bc988f9
alkrivosheev <alkrivosheev@gmail.com>
2025-04-14 10:27:07 +0300

    change color

--- src/main/java/ru/job4j/sqch8ex1/MainController.java
+++ src/main/java/ru/job4j/sqch8ex1/MainController.java
@@ -3,13 +3,14 @@ package ru.job4j.sqch8ex1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home(Model page) {
+    public String home(@RequestParam String color, Model page) {
         page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
